88f735d64c0efe55baaf578ae523f53fe8af4118
jnyim <jianany@andrew.cmu.edu>
2018-02-28 00:21:33 -0500

    start list

--- app/src/main/java/com/example/linxing/create/IngredientActivity.java
+++ app/src/main/java/com/example/linxing/create/IngredientActivity.java
@@ -2,6 +2,8 @@ package com.example.linxing.create;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by jiana on 2018/2/27.
@@ -12,5 +14,19 @@ public class IngredientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingredient);
+
+        String[] fake = new String[5];
+        for (int i = 0; i < 5; i++) {
+            fake[i] = "123";
+        }
+        // Set the listview to display all questions
+        ListView listView = (ListView)findViewById(R.id.recipe_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                fake);
+
+        listView.setAdapter(adapter);
     }
 }
