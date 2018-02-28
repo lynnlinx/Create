e5f25f336f423ef3a0b71f4091bb50ad4e43faac
jnyim <jianany@andrew.cmu.edu>
2018-02-27 21:47:21 -0500

    JN: half of register

--- app/src/main/java/com/example/linxing/create/LoginActivity.java
+++ app/src/main/java/com/example/linxing/create/LoginActivity.java
@@ -2,16 +2,18 @@ package com.example.linxing.create;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
 
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by jiana on 2018/2/27.
+ */
 
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth myAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        finish();
-        startActivity(new Intent(this, RegisterActivity.class));
+        setContentView(R.layout.login);
     }
 }
---
+++ app/src/main/java/com/example/linxing/create/UserProfile.java
@@ -0,0 +1,12 @@
+package com.example.linxing.create;
+
+/**
+ * Created by jiana on 2018/2/27.
+ */
+
+public class UserProfile {
+    public String email;
+    public UserProfile(String email){
+        this.email = email;
+    }
+}
