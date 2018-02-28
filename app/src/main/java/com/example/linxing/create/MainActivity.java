1f526a96c701e56b86ec0f20fb3bcee68ae75aed
lxing <lxing@andrew.cmu.edu>
2018-02-28 00:05:16 -0500

    Finish ingredient page XML design

--- app/src/main/java/com/example/linxing/create/MainActivity.java
+++ app/src/main/java/com/example/linxing/create/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.linxing.create;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        finish();
-        startActivity(new Intent(this, RegisterActivity.class));
+        setContentView(R.layout.ingredient);
+        //finish();
+        //startActivity(new Intent(this, RegisterActivity.class));
     }
 }
