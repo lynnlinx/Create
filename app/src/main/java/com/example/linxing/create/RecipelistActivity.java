8ae40f3223dc24e6111addb2de8c3624a514a775
Xingzhi Wang <xingzhiw@outlook.com>
2018-02-28 16:37:49 -0500

    add back button

--- app/src/main/java/com/example/linxing/create/RecipelistActivity.java
+++ app/src/main/java/com/example/linxing/create/RecipelistActivity.java
@@ -1,5 +1,6 @@
 package com.example.linxing.create;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -31,6 +33,21 @@ public class RecipelistActivity extends AppCompatActivity {
 
         myadapter=new MyCustomAdapter(listnewsData);
         recilist.setAdapter(myadapter);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Recipes");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new         Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
     }
 
     private class MyCustomAdapter extends BaseAdapter {
