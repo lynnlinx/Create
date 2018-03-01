5c566177fc956a5a3d49f2559797d6ccd8fa988e
jnyim <jianany@andrew.cmu.edu>
2018-02-28 22:45:24 -0500

    ingredient

--- app/src/main/java/com/example/linxing/create/DetailRecipeActivity.java
+++ app/src/main/java/com/example/linxing/create/DetailRecipeActivity.java
@@ -1,7 +1,10 @@
 package com.example.linxing.create;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by jiana on 2018/2/27.
@@ -14,5 +17,21 @@ public class DetailRecipeActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailrecipe);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle("");
+
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(getApplicationContext(),RecipelistActivity.class));
+                }
+            });
+        }
     }
 }
